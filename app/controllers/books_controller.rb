f6be091bc3e863d8dca4b09463748ddf52e92273
sumio96k <106654738+sumio96k@users.noreply.github.com>
2022-06-13 02:49:00 +0000

    [update] 4-25

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,22 @@
 class BooksController < ApplicationController
   def index
+    @book = Book.new #new投稿の部分
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to user_path(current_user.id)
   end
 
   def show
   end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
