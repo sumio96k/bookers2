f8bbf068cc7dc6712568d43399188a42995afc94
sumio96k <106654738+sumio96k@users.noreply.github.com>
2022-06-14 13:25:57 +0000

    [update] 4-25

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,12 +8,18 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      @user = User.find(current_user.id)
+      render :index
+    end
   end
 
   def show
     @book = Book.find(params[:id])
+    @books = Book.new
     @user = @book.user
   end
 
--- app/models/book.rb
+++ app/models/book.rb
@@ -4,4 +4,7 @@ class Book < ApplicationRecord
 
   belongs_to :user
 
+  validates :title, presence: true
+  validates :body, presence: true
+
 end
