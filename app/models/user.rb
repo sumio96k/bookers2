077982322775dd3c3c303b778776c9568913f7a8
sumio96k <106654738+sumio96k@users.noreply.github.com>
2022-06-22 07:49:47 +0000

    [update] kadai4

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,13 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
-
+  # 自分がフォローする（与フォロー）側の関係性
   has_many :active_relationships, class_name: "Relationship",foreign_key: :follower_id, dependent: :destroy
-  has_many :passive_relationships, class_name: "Relationship",foreign_key: :followed_id, dependent: :destroy
+  # 与フォロー関係を通じて参照→(has_many)自分がフォローしている人
   has_many :followings, through: :active_relationships, source: :followed
+  # 自分がフォローされる（被フォロー）側の関係性
+  has_many :passive_relationships, class_name: "Relationship",foreign_key: :followed_id, dependent: :destroy
+  # 被フォロー関係を通じて参照→(has_many)自分をフォローしている人
   has_many :followers, through: :passive_relationships, source: :follower
 
   has_one_attached :profile_image
@@ -19,12 +22,12 @@ class User < ApplicationRecord
   validates :introduction, length: { maximum: 50 }
 
 
-  def follow(user_id)
-    active_relationships.create(followed_id: user_id)
+  def follow(user)
+    active_relationships.create(followed_id: user.id)
   end
 
-  def unfollow(user_id)
-    active_relationships.find_by(followed_id: user_id).destroy
+  def unfollow(user)
+    active_relationships.find_by(followed_id: user.id).destroy
   end
 
   def following?(user)
