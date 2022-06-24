7bffa2a7cbee933b84ffc88ff305c5497c1d95dc
sumio96k <106654738+sumio96k@users.noreply.github.com>
2022-06-24 15:45:22 +0000

    [update] kadai6

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,27 +1,22 @@
 class BookCommentsController < ApplicationController
 
   def create
-  @book = Book.find(params[:book_id])
-  @book_comment = BookComment.new
-  @comment = current_user.book_comments.new(book_comments_params)
-  @comment.book_id = @book.id
-  @comment.save
-  # flash.now[:notice] = "コメントの投稿に成功しました。"
-  # render 'book_comments/index'
-  # redirect_back(fallback_location: root_path)
+    book = Book.find(params[:book_id])
+    @comment = current_user.book_comments.new(book_comment_params)
+    @comment.book_id = book.id
+    @comment.save
   end
 
   def destroy
-    BookComment.find_by(params[:id], book_id: params[:book_id]).destroy
-    @book = Book.find(params[:book_id])
-    @book_comment = BookComment.new
-    # render :index
-    # redirect_back(fallback_location: root_path)
+    # BookComment.find_by(params[:id], book_id: params[:book_id]).destroy
+    # @book = Book.find(params[:book_id])
+    @comment = BookComment.find(params[:id])
+    @comment.destroy
   end
 
   private
 
-  def book_comments_params
+  def book_comment_params
     params.require(:book_comment).permit(:comment)
   end
 
